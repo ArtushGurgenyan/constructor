0650d2ec55cfb51c2b29a48f518d7ad5b0b9f493
ArtusGurgenyan <artushgurgenyan1@gmail.com>
2025-01-24 12:50:00 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+let constructor = document.querySelector(".constructor");
+let result = document.querySelector(".result");
+let button = document.querySelector(".createBtn");
+let select = document.querySelector("select");
+
+let choosen = "div"; 
+let constructorElements = new Map();  
+let clickedElement = null;  
+let data = []; 
+
+
+select.addEventListener("change", (e) => {
+    choosen = e.target.value;
+});
+
+
+function createDomElement(data) {
+    const newElement = {
+        id: data.length,
+        type: "div",
+        context: choosen,
+        parentId: 1,  
+    };
+
+    data.push(newElement);  
+
+    const item = data[data.length - 1]; 
+    let el = document.createElement(item.type); 
+    el.classList.add("option");              
+    el.classList.add(item.context);         
+    el.textContent = item.context;          
+    el.dataset.id = item.id; 
+
+    constructorElements.set(item.id, el); 
+    constructor.appendChild(el); 
+
+
+    el.addEventListener("mousedown", (e) => {
+        clickedElement = el;  
+    });
+}
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    createDomElement(data);  
+});
+
+
+
+
+constructor.addEventListener("mouseup", (e) => {
+    e.stopPropagation();
+
+    if (clickedElement) {
+
+        if (!result.contains(clickedElement)) {
+            result.appendChild(clickedElement);
+            clickedElement.parentId = 2; 
+        }
+    }
+
+    clickedElement = null;
+});
+
+result.addEventListener("mouseup", (e) => {
+    e.stopPropagation();
+
+    if (clickedElement && clickedElement instanceof Node) {
+        let targetElem = e.target;
+
+        if (targetElem !== result && targetElem.textContent === "div") {
+            targetElem.appendChild(clickedElement);
+            clickedElement.parentId = 2; 
+        }
+    }
+
+    clickedElement = null; 
+});
